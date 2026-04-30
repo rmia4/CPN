2e0c635b08d3ff31a4179c69f93928413adeb738
rmia4 <psd020517@naver.com>
2026-04-30 13:24:07 +0900

    feat:grok index 생성

--- src/main/java/com/example/proj/domain/grok/GrokController.java
+++ src/main/java/com/example/proj/domain/grok/GrokController.java
@@ -1,4 +1,21 @@
 package com.example.proj.domain.grok;
 
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
 public class GrokController {
+    private final GrokService grokService;
+
+
+    @GetMapping("/grok")
+    public String grokMain() {
+
+
+        return "pages/grok/grokIndex";
+    }
+
 }
--- src/main/java/com/example/proj/domain/grok/GrokService.java
+++ src/main/java/com/example/proj/domain/grok/GrokService.java
@@ -1,4 +1,12 @@
 package com.example.proj.domain.grok;
 
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class GrokService {
+
+
 }
