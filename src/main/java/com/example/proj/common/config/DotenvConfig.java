1eb6abbccf8e92163fd61b46e26fe5c298e82320
rmia4 <psd020517@naver.com>
2026-05-21 14:47:17 +0900

    fix:dot

--- src/main/java/com/example/proj/common/config/DotenvConfig.java
+++ src/main/java/com/example/proj/common/config/DotenvConfig.java
@@ -9,6 +9,8 @@ public class DotenvConfig {
 
     @Bean
     public Dotenv dotenv() {
-        return Dotenv.load();
+        return Dotenv.configure()
+                .ignoreIfMissing()
+                .load();
     }
 }
